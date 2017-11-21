01ef303cc395efaef6e337b9cd3654cdcd98c068
Aleksandar Milošević <dev@sasxa.net>
2017-11-21 02:44:56 +0100

    schematics(express): Add schematic options

--- src/schematics/express/index.ts
+++ src/schematics/express/index.ts
@@ -1,6 +1,16 @@
 import { Rule, SchematicContext, Tree, apply, branchAndMerge, chain, filter, mergeWith, move, template, url } from '@angular-devkit/schematics';
 
-function addExpressDependencies() {
+import { exec } from 'child_process';
+import { sortKeys } from '../utils';
+
+const packages = [
+  'compression',
+  'connect-history-api-fallback',
+  'express',
+  'yargs'
+];
+
+function addDependencies() {
   return (host: Tree, context: SchematicContext) => {
     if (!host.exists('package.json')) { return host; }
 
@@ -9,52 +19,62 @@ function addExpressDependencies() {
     if (!json[ 'devDependencies' ]) {
       json[ 'devDependencies' ] = {};
     }
-    if (!json[ 'devDependencies' ][ 'compression' ]) {
-      json[ 'devDependencies' ][ 'compression' ] = 'latest';
-    }
-    if (!json[ 'devDependencies' ][ 'express' ]) {
-      json[ 'devDependencies' ][ 'express' ] = 'latest';
-    }
-    if (!json[ 'devDependencies' ][ 'connect-history-api-fallback' ]) {
-      json[ 'devDependencies' ][ 'connect-history-api-fallback' ] = 'latest';
-    }
-    if (!json[ 'devDependencies' ][ 'yargs' ]) {
-      json[ 'devDependencies' ][ 'yargs' ] = 'latest';
-    }
+
+    packages.forEach(name => {
+      if (!json[ 'devDependencies' ][ name ]) {
+        json[ 'devDependencies' ][ name ] = '*';
+      }
+    });
+
+    json[ 'devDependencies' ]
+      = sortKeys(json[ 'devDependencies' ]);
+
     host.overwrite('package.json', JSON.stringify(json, null, 2));
     return host;
   }
 }
 
-function addAngularGuiCommand() {
+function installDependencies(options: ExpressOptions) {
   return (host: Tree, context: SchematicContext) => {
+    if (options.npmInstall) {
+      const installed
+        = packages.every(name =>
+          host.exists(`node_modules/${ name }/package.json`));
 
+      if (!installed) {
+        exec('npm install', { cwd: host[ '_host' ][ '_root' ] });
+      }
+    }
     return host;
   }
 }
 
-export interface ExpressOptions {
-  name?: string;
+interface ExpressOptions {
+  name: string;
+  npmInstall: boolean;
+  port: number;
+  rootDir: string;
 }
 
 export default function (options: ExpressOptions): Rule {
   const scriptSource = apply(url('./files'), [
     filter(path => path.endsWith('.js')),
     template({ ...options }),
-    move('./')
+    move(options.rootDir)
   ]);
 
   const commandSource = apply(url('./files'), [
     filter(path => path.endsWith('.sh')),
     template({ ...options }),
-    move('.angular-gui/commands')
+    move(`${ options.rootDir }/commands`)
   ]);
 
   return chain([
-    addExpressDependencies(),
+    addDependencies(),
     branchAndMerge(chain([
       mergeWith(scriptSource),
       mergeWith(commandSource),
     ])),
+    installDependencies(options)
   ]);
 }
