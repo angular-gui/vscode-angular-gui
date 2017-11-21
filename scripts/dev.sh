a6d8ae9ef7e457101bcdfa215889e13792739283
Aleksandar Milošević <dev@sasxa.net>
2017-11-21 04:13:22 +0100

    chore: Update development scripts

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,8 @@
-echo "Building extension \"ng-gui\" in watch mode"
+echo "Building extension \"angular-gui\" in watch mode"
+
+sh scripts/schematics.sh
 
 concurrently --kill-others \
   "webpack --config webpack.local.js --display minimal -w" \
+  "tsc -p ./tsconfig.schematics.json --watch" \
   "nodemon out/local.js"
-
--- scripts/schematics.sh
+++ scripts/schematics.sh
@@ -2,5 +2,6 @@ echo "Building schematics for \"angular-gui\" extension"
 
 rm -rf schematics
 tsc -p ./tsconfig.schematics.json
+
 cp -r src/schematics/* schematics
 rm -rf schematics/**/*.ts schematics/*.ts
