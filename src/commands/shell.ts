f6cbed31827e3edb3a1d2916e985838b52c271c0
Aleksandar Milošević <dev@sasxa.net>
2017-11-24 07:52:02 +0100

    chore: Cleanup shell command options

--- src/commands/shell.ts
+++ src/commands/shell.ts
@@ -4,24 +4,7 @@ export const shell = {
   name: 'shell',
   description: 'Create custom shell commands.',
 
-  availableOptions: [
-    {
-      name: 'app',
-      type: String,
-      aliases: [ 'a' ],
-      description: 'Specifies app name or index to use.'
-    },
-    {
-      name: 'command',
-      type: String,
-      description: 'Name of the shell command or program to execute.'
-    },
-    {
-      name: 'file',
-      type: 'Path',
-      description: `Specify a file to be used as command argument.`
-    },
-  ]
+  availableOptions: []
     .sort(sort('asc', o => o.name))
     .map(type)
 };
